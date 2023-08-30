d1a1036b6c94cfb5ea62729537c2cd25bf47b6b7
DavidEnriqueNieves <davidenriquenieves@gmail.com>
2023-08-30 01:05:23 -0400

    got some sort of transition going?

--- src/MainCarousel.js
+++ src/MainCarousel.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { CSSTransition } from "react-transition-group";
 import ReactDOM from "react-dom";
 import brotherhood from "./homepage_imgs/everyone_rocket_uspcaled.jpg";
 import professionalism from "./homepage_imgs/everyone_professional_peter.jpg";
@@ -26,55 +27,81 @@ class MainCarousel extends React.Component {
     super(props);
     this.state = {
       current_showing: 0,
+      inProp: true,
     };
     this.current_showing = 0;
   }
 
+  toggle = () => {
+    this.setState((prevState) => ({
+      inProp: !prevState.inProp,
+    }));
+  };
+
+  componentDidMount() {
+    // set a timer for 3 seconds to change properties of carousel-container
+    console.log("componentDidMount is being called");
+    console.log("Timing out for 3 seconds");
+    this.timeout();
+  }
+
+  timeout = () =>
+    setTimeout(() => {
+      console.log(" this.current_showing is ");
+      console.log(this.current_showing);
+      console.log(this.CarouselContent[this.current_showing]);
+      console.log(this.CarouselContent[this.current_showing].image);
+      var next_showing = (this.current_showing + 1) % 3;
+      console.log("next_showing is " + next_showing);
+
+      this.setState({ current_showing: next_showing, inProp: false });
+    }, 3000);
+
   render() {
     var gears = GearUtils.getGearsMainCarousel();
-
-    // set a timer for 3 seconds to change properties of carousel-container
-    // this.timeout = setTimeout(() => {
-    // console.log(" this.current_showing is ");
-    // console.log(this.current_showing);
-    // console.log(this.CarouselContent[this.current_showing]);
-    // console.log(this.CarouselContent[this.current_showing].image);
-    // var next_showing = (this.current_showing + 1) % 3;
-
-    // var carouselContainer = document.getElementById("carousel-container");
-
-    // carouselContainer.style.backgroundImage = `url(${this.CarouselContent[next_showing].image})`;
-    // var carouselSlogan = document.getElementById("CarouselSlogan");
-    // carouselSlogan.innerHTML = `${this.CarouselContent[next_showing].slogan}`;
-    // this.current_showing = next_showing;
-    // }, 3000);
+    console.log("Rerendering with value of ", this.state.current_showing);
+    console.log(
+      "Image is ",
+      this.CarouselContent[this.state.current_showing].image
+    );
+    console.log("InProp is ", this.state.inProp);
+    this.timeout();
 
     return (
-      <div
-        id="carousel-container"
-        style={{
-          backgroundImage: `url(${
-            this.CarouselContent[this.current_showing].image
-          })`,
-        }}
-      >
-        <div id="page-title" className="centered fade-in-bottom">
-          <div className="theta-tau-title">
-            <div className="theta-title"> Θ </div>
-            <div className="tau-title">Τ</div>
-          </div>
+      <div>
+        <CSSTransition
+          in={this.state.inProp}
+          timeout={3000}
+          classNames="carousel"
+          onEntered={this.toggle}
+        >
+          <div
+            id="carousel-container"
+            style={{
+              backgroundImage: `url(${
+                this.CarouselContent[this.current_showing].image
+              })`,
+            }}
+          >
+            <div id="page-title" className="centered fade-in-bottom">
+              <div className="theta-tau-title">
+                <div className="theta-title"> Θ </div>
+                <div className="tau-title">Τ</div>
+              </div>
 
-          <div id="chapter-title" className="hor-flex">
-            <div className="chapter-letters">ΨΕ </div>
-            <div>Chapter</div>
-          </div>
-          <div id="CarouselSlogan" className="carousel-slogan">
-            {this.CarouselContent[this.current_showing].slogan}
-          </div>
+              <div id="chapter-title" className="hor-flex">
+                <div className="chapter-letters">ΨΕ </div>
+                <div>Chapter</div>
+              </div>
+              <div id="CarouselSlogan" className="carousel-slogan">
+                {this.CarouselContent[this.current_showing].slogan}
+              </div>
 
-          <div className="centered icon"></div>
-        </div>
-        {gears}
+              <div className="centered icon"></div>
+            </div>
+            {gears}
+          </div>
+        </CSSTransition>
       </div>
     );
   }
